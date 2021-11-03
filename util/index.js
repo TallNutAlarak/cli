f141071f48543bc4356c1eedd2c673af8478815e
王志伟 <1194291667@qq.com>
2021-11-03 16:16:37 +0800

    creat object add dependencies install

--- util/index.js
+++ util/index.js
@@ -1,7 +1,19 @@
 import fs from "fs-extra";
 import path from "path";
 import ejs from "ejs";
+import { ChildProcess, spawn } from "child_process";
 // https://github1s.com/zhangbanghui/zhang-cli/blob/master/lib/utils/utils.js
+
+export const commandSpan = (...args) => {
+    return new Promise((resolve, reject) => {
+        const childProcess = spawn(...args);
+        childProcess.stdout.pipe(process.stdout);
+        childProcess.stderr.pipe(process.stderr);
+        childProcess.on("close", resolve);
+        childProcess.on("error", reject);
+    });
+};
+
 export const compile = (fileName, data) => {
     const filePath = `../../templates/${fileName}`;
     const absPath = path.resolve(decodeURI(import.meta.url.slice(7)), filePath);
